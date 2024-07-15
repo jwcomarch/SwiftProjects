da92cf00c50b27fa52be94c1630fb933cc4f364e
jakubw <jakub.wlodarski@stazysta.comarch.pl>
2024-07-15 12:56:25 +0200

    Challenge4 - Implemented app's functionality

--- HWS/MilestoneProject1/MilestoneProject1/ViewController.swift
+++ HWS/MilestoneProject1/MilestoneProject1/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var flags = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Flags App"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for i in items{
+            if i.hasSuffix("@2x.png"){
+                flags.append(String(i.dropLast(7)))
+            }
+        }
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = flags[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Details") as? DetailsVC {
+            vc.selectedFlag = flags[indexPath.row] + "@2x"
+            vc.title = flags[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
