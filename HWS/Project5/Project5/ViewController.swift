6eb96c3001364c67107df950c6bcecbe63896afd
jwcomarch <jakub.wlodarski@stazysta.comarch.pl>
2024-07-16 16:50:51 +0200

    Challenge5 - Implemented app's functionality and cleaned up code

--- HWS/Project5/Project5/ViewController.swift
+++ HWS/Project5/Project5/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(restartGame))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
             if let startWord = try? String(contentsOf: startWordsURL){
@@ -32,6 +33,10 @@ class ViewController: UITableViewController {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
+    }
+    
+    @objc func restartGame() {
+        startGame()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -62,35 +67,73 @@ class ViewController: UITableViewController {
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
         
-        let errorTitle: String
-        let errorMessage: String
-        
-        if isPossible(word: lowerAnswer){
-            if isOriginal(word: lowerAnswer){
-                if isReal(word: lowerAnswer){
-                    usedWords.insert(answer, at: 0)
-                    
-                    let indexPath = IndexPath(row: 0, section: 0)
-                    tableView.insertRows(at: [indexPath], with: .automatic)
-                    
-                    return
-                } else {
-                    errorTitle = "Word not recognized"
-                    errorMessage = "You can't input made up words!"
-                }
-            } else {
-                errorTitle = "Word already used"
-                errorMessage = "Be more original!"
-            }
-        } else {
-            guard let title = title else { return }
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from \(title.lowercased())!"
+        let wordCheck = isWordAcceptable(word: lowerAnswer)
+        if wordCheck.2 {
+            usedWords.insert(answer, at: 0)
+            let indexPath = IndexPath(row: 0, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+            
+            print("2")
+            return
         }
+        let errorTitle = wordCheck.0
+        let errorMessage = wordCheck.1
         
+        print("3")
         let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
+    }
+    
+    func isWordAcceptable(word: String) -> (String, String, Bool){
+        var errorTitle = "Error"
+        var errorMsg = "Unexpected error occurred. Please contact app developer."
+        var isAcceptable = false
+        guard let title = title else { return (errorTitle, errorMsg, isAcceptable)}
+        
+        let wordChecks: [Bool] = [
+            isLongEnough(word: word),
+            isPossible(word: word),
+            isOriginal(word: word),
+            isNotTitle(word: word),
+            isReal(word: word),]
+        
+        for i in wordChecks.indices {
+            if !wordChecks[i] {
+                print("jest w switchu")
+                switch(i){
+                case 0:
+                    print("case 0")
+                    errorTitle = "Word too short"
+                    errorMsg = "Word must be at least 3 letters long."
+                case 1:
+                    print("case 1")
+                    errorTitle = "Word not possible"
+                    errorMsg = "You can't spell that word from \(title.lowercased())!"
+                case 2:
+                    print("case 2")
+                    errorTitle = "Word already used"
+                    errorMsg = "Be more original!"
+                case 3:
+                    print("case 4")
+                    errorTitle = "Word not allowed"
+                    errorMsg = "Word inputted must not be the same as the title."
+                case 4:
+                    print("case 3")
+                    errorTitle = "Word not recognized"
+                    errorMsg = "You can't input made up words!"
+                default: break
+                }
+                return (errorTitle, errorMsg, isAcceptable)
+            }
+        }
+        print("1")
+        isAcceptable = true
+        return (errorTitle, errorMsg, isAcceptable)
+    }
+    
+    func isLongEnough(word: String) -> Bool {
+        return word.utf16.count > 2
     }
     
     func isPossible(word: String) -> Bool {
@@ -112,9 +155,16 @@ class ViewController: UITableViewController {
     
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
+        let wordLength = word.utf16.count
+        let range = NSRange(location: 0, length: wordLength)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         
-        return misspelledRange.location == NSNotFound
+        return (misspelledRange.location == NSNotFound
+                && wordLength > 2
+                && word != title?.lowercased())
+    }
+    
+    func isNotTitle(word: String) -> Bool {
+        return word != title!.lowercased()
     }
 }
