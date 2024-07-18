e9c9658685ba91170338682f28cb4e59fce2437d
jwcomarch <jakub.wlodarski@stazysta.comarch.pl>
2024-07-18 12:56:38 +0200

    Initial commit

---
+++ HWS/Project9/Project7/DetailVC.swift
@@ -0,0 +1,39 @@
+//
+//  DetailVC.swift
+//  Project7
+//
+//  Created by Jakub Włodarski on 17/07/2024.
+//
+
+import UIKit
+import WebKit
+
+class DetailVC: UIViewController {
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let detailItem = detailItem else { return }
+        
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+}
