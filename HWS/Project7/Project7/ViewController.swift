f64d664719b4d27b5ab2fe3523f4607f13ec2872
jwcomarch <jakub.wlodarski@stazysta.comarch.pl>
2024-07-18 14:11:27 +0200

    Implemented challenge 9 functionality to Project 1, 7, 8

--- HWS/Project7/Project7/ViewController.swift
+++ HWS/Project7/Project7/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var allPetitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Whitehouse Petitions"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "doc.text.magnifyingglass"), style: .plain, target: self, action: #selector(filterResults))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
         
         let urlString: String
         
@@ -31,7 +36,56 @@ class ViewController: UITableViewController {
         showError()
     }
     
-    func showError(){
+    @objc func filterResults() {
+        let ac = UIAlertController(title: "Filter results", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Search", style: .default) {
+            [self, weak ac] _ in
+            guard let userInput = ac?.textFields?[0].text else { return }
+            performSelector(inBackground: #selector(submit), with: userInput)
+            //self?.submit(userInput)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) {_ in
+            self.undoFilter()
+        }
+        
+        ac.addAction(submitAction)
+        ac.addAction(cancelAction)
+        present(ac, animated: true)
+    }
+    
+    @objc func submit(_ input: String) {
+        if input.isEmpty {
+            undoFilter()
+            return
+        }
+        var tempArr = [Petition]()
+        for p in petitions {
+            if p.title.contains(input) || p.body.contains(input){
+                tempArr.append(p)
+            }
+        }
+        petitions = tempArr
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func undoFilter(){
+        petitions = allPetitions
+        tableView.reloadData()
+    }
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "The data used in the app comes from We The People API of the Whitehouse.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func showError() {
         let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
@@ -42,6 +96,7 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            allPetitions = petitions
             tableView.reloadData()
         }
     }
