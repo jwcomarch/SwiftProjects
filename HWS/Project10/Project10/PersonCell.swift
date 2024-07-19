400b15574c516b1aee331fceacd9222afa00ee47
jwcomarch <jakub.wlodarski@stazysta.comarch.pl>
2024-07-19 08:20:05 +0200

    Initial commit

---
+++ HWS/Project10/Project10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Jakub Włodarski on 18/07/2024.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
