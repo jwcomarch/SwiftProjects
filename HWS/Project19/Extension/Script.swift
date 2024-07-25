0fe57466c00559e07e2519b7a0ae5970f25306c3
jwcomarch <jakub.wlodarski@stazysta.comarch.pl>
2024-07-25 11:06:28 +0200

    Implemented challenge 19 functionality

---
+++ HWS/Project19/Extension/Script.swift
@@ -0,0 +1,8 @@
+//
+//  Script.swift
+//  Extension
+//
+//  Created by Jakub Włodarski on 24/07/2024.
+//
+
+import Foundation
---
+++ HWS/Project19/Extension/ScriptsVC.swift
@@ -0,0 +1,29 @@
+//
+//  ScriptsVC.swift
+//  Extension
+//
+//  Created by Jakub Włodarski on 24/07/2024.
+//
+
+import UIKit
+
+class ScriptsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
